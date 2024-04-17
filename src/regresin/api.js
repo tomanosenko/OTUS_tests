c7b8827ad131ec4afb83be80e3fbc5ac8e55759d
Damir Rysaev <freepad@ya.ru>
2024-04-17 15:57:20 +0300

    complete CRUD user

--- src/regresin/api.js
+++ src/regresin/api.js
@@ -10,6 +10,7 @@ async function fetchUsers() {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 async function fetchUser(id) {
   const response = await fetch(`${baseUrl}/users/${id}`)
   return {
@@ -19,9 +20,70 @@ async function fetchUser(id) {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
+async function updateUser(id, data) {
+  const response = await fetch(`${baseUrl}/users/${id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  return {
+    status: response.status,
+    headers: response.headers,
+    data: await response.json(),
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+async function putUser(id, data) {
+  const response = await fetch(`${baseUrl}/users/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  return {
+    status: response.status,
+    headers: response.headers,
+    data: await response.json(),
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+async function deleteUser(id) {
+  const response = await fetch(`${baseUrl}/users/${id}`, {
+    method: 'DELETE',
+  })
+  return {
+    status: response.status,
+    headers: response.headers,
+  }
+}
+
+async function createUser(data) {
+  const response = await fetch(`${baseUrl}/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  return {
+    status: response.status,
+    headers: response.headers,
+    data: await response.json(),
+  }
+}
+
 ;(async () => {
   try {
-    const response = await fetchUser(2)
+    const response = await createUser({
+      name: 'Damir Rysaev',
+      job: 'TechLead',
+    })
     console.log('status', response.status)
     console.log('data', response.data)
   } catch (error) {
