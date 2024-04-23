12c2876cd6a03098e7f4615dfcaffef7259e3499
Damir Rysaev <freepad@ya.ru>
2024-04-23 18:13:29 +0300

    add dotenv

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,27 +1,33 @@
+const config = {
+  baseURL: process.env.TEST_BASE_API_URL,
+  username: process.env.TEST_DUMMYJSON_USERNAME,
+  password: process.env.TEST_DUMMYJSON_PASSWORD,
+}
+
 describe('Auth', () => {
   it('Success login', async () => {
-    const response = await fetch('https://dummyjson.com/auth/login', {
+    const response = await fetch(`${config.baseURL}/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        username: 'kminchelle',
-        password: '0lelplR',
+        username: config.username,
+        password: config.password,
         expiresInMins: 30,
       }),
     })
     const data = await response.json()
 
     expect(response.status).toEqual(200)
-    expect(data.username).toBe('kminchelle')
+    expect(data.username).toBe(config.username)
     expect(data.token).toBeTruthy()
   })
 
   it('Failed login', async () => {
-    const response = await fetch('https://dummyjson.com/auth/login', {
+    const response = await fetch(`${config.baseURL}/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        username: 'kminchelle',
+        username: config.username,
         password: 'wrongpassword',
         expiresInMins: 30,
       }),
